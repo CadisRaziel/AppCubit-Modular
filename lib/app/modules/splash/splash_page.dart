56d2800007d0149cef1ebff0baddfdc01bb1cc94
vitu <vitorcsharp@hotmail.com>
2023-08-11 11:10:34 -0300

    verificando se o usuario esta logado ou não na splash page

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,7 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+  //?O firebaseAUth tenta mante o usuario logado ao maximo
+  //*O firebase auth fica escutando quando a gente faz o login ou não (é um listener)
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if(user == null){
+        //*Se o usuário for nulo ele nao esta logado, ele vai para a tela de login
+        Modular.to.navigate('/login');
+      } else {
+        //*Se o usuário não for nulo ele esta logado, ele vai para a tela de home
+        Modular.to.navigate('/home');
+      }
+     });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
