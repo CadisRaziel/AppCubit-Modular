27b9d94d4cf7cb447449594600fb832755947018
vitu <vitorcsharp@hotmail.com>
2023-08-11 10:53:58 -0300

    construindo a splash page

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:jobtimer/app/modules/core/ui/job_timer_icons.dart';
 
 class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
 
-  const SplashPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Center(child: Container(
-            child: Icon(JobTimerIcons.angle_double_right),
-           )),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color(0xFF0092B9),
+          Color(0xFF0167B2),
+        ])),
+        child: Center(
+          child: Image.asset("assets/images/logo.png"),
+        ),
+      ),
+    );
   }
 }
