0d0c22b73e74f038511a3efcf559e48585a479d5
vitu <vitorcsharp@hotmail.com>
2023-08-11 10:58:44 -0300

    criando pagina e modulo dessas paginas para login e homepage

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:jobtimer/app/modules/home/home_module.dart';
+import 'package:jobtimer/app/modules/login/login_module.dart';
 import 'package:jobtimer/app/modules/splash/splash_page.dart';
 
 class AppModule extends Module {
@@ -8,6 +10,8 @@ class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
     // ChildRoute('/', child: (_, __) => Container());
-    ChildRoute('/', child: (context, args) => const SplashPage()),
+    ChildRoute('/', child: (context, args) => const SplashPage()), //*-> Para adicionar uma nova página
+    ModuleRoute('/login', module: LoginModule()), //*-> Para adicionar um novo módulo (como nosso modulo de login)
+    ModuleRoute('/home', module: HomeModule()), //*-> Para adicionar um novo módulo (como nosso modulo de login)
   ];
 }
