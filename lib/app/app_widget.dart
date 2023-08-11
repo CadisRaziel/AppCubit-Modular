d4fa1ddf15694a69ef589a5a305671f05e8b3be1
vitu <vitorcsharp@hotmail.com>
2023-08-11 10:37:37 -0300

    configurando o themeData

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:jobtimer/app/modules/core/ui/app_config_ui.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -10,6 +11,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp.router(
       title: "Job Timer",      
       builder: Asuka.builder,
+      theme: AppConfigUi.theme,
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
     );
