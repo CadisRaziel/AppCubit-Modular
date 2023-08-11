705ea016ce70cd7cae66f014540b22dccfd1e768
vitu <vitorcsharp@hotmail.com>
2023-08-11 11:20:24 -0300

    criando tela de login

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-  const LoginPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('LoginPage'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color(0xFF0092B9),
+          Color(0xFF0167B2),
+        ])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/logo.png"),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              SizedBox(
+                height: 49,
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[200],
+                    
+                  ),
+                  onPressed: () {},
+                  child: Image.asset("assets/images/google.png"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
